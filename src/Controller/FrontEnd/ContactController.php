a179e8220c6962b6f52acb39be6a2dcdc1f647cb
ksami <khemsisami@gmail.com>
2020-11-04 20:09:03 +0100

    Updating ContactController

--- src/Controller/FrontEnd/ContactController.php
+++ src/Controller/FrontEnd/ContactController.php
@@ -2,18 +2,17 @@
 
 namespace App\Controller\FrontEnd;
 
-use App\Email\SwiftMailerEmail;
 use App\Entity\Contact;
 use App\Form\ContactType;
 use App\Form\Handler\ContactHandler;
 use App\Manager\ContactManager;
 use App\Manager\PartnerManager;
-use phpDocumentor\Reflection\Types\This;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response as ResponseAlias;
-use Symfony\Component\Mailer\Bridge\Sendgrid\Transport\SendgridTransportFactory;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ContactController extends AbstractController
@@ -42,35 +41,32 @@ class ContactController extends AbstractController
      * @Route("/contact", name="app_contact")
      *
      * @param Request $request
+     * @param MailerInterface $mailer
      * @return ResponseAlias
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
-    public function contact(Request $request, \Swift_Mailer $mailer)
+    public function contact(Request $request, MailerInterface $mailer)
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
         $handler = new ContactHandler($form, $request, $this->em);
+        
         if ($handler->process()) {
-
-            $transport = (new \Swift_SmtpTransport('smtp.sendgrid.net', 587))
-                ->setUsername('contact@hiboo.live')
-                ->setPassword('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');
-
-            $mailered = new \Swift_Mailer($transport);
-
-            $message = (new \Swift_Message())
-                ->setSubject('Here should be a subject')
-                ->setFrom(['contact@hiboo.live'])
-                ->setTo(['tsivika@gmail.com' => 'nouveau mail '])
-                ->setCc([
-                    'contactdiaryko@gmail.com' => 'Product manager'
-                ]);
-
-            $message->setBody('<html><body><p>Welcome to at home</p></body></html>','text/html');
-            $mailered->send($message);
-
-            
+            $email = (new Email())
+                ->from('contact@hiboo.live')
+                ->to('test+test+test@gmail.com')
+                //->cc('cc@example.com')
+                //->bcc('bcc@example.com')
+                //->replyTo('fabien@example.com')
+                //->priority(Email::PRIORITY_HIGH)
+                ->subject('Time for Symfony Mailer!')
+                ->text('Sending emails is fun again!')
+                ->html('<p>See Twig integration for better HTML integration!</p>');
+    
+            $mailer->send($email);
 
             $this->addFlash('success', 'Demande de contact prise en compte. Un email sera envoyé au responsable.');
+            
             return $this->redirectToRoute('app_contact');
         }
 
