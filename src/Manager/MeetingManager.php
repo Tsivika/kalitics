3408783af42107ae0f49ce772cd1a02c7b892fab
ANDRIAMIFIDY Faniloniaina <tsivika@gmail.com>
2020-12-04 05:28:20 +0100

    feedback-pdf-without-sliders-browser-profil

--- src/Manager/MeetingManager.php
+++ src/Manager/MeetingManager.php
@@ -400,7 +400,9 @@ class MeetingManager extends BaseManager
         ];
 
         foreach ($meetingUser->getParticipants() as $row) {
-            $this->emailService->sendEmail($_ENV['CONTACT_MAIL'], $row->getEmail(), 'Hiboo: Invitation à une réunion.', $template, $context) ;
+            if ($row->getEmail()) {
+                $this->emailService->sendEmail($_ENV['CONTACT_MAIL'], $row->getEmail(), 'Hiboo: Invitation à une réunion.', $template, $context) ;
+            }
         }
     }
 }
