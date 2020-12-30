309a9fe382f7e6ffc7bc0610aacd97024ff813a7
Fanilo <fanilo@hightao-mg.com>
2020-12-30 05:51:36 +0100

    feedback-5

--- assets/js/hiboo.js
+++ assets/js/hiboo.js
@@ -130,8 +130,17 @@ $(document).ready(function() {
 			$('#mainNav').removeClass('navbar-light');
 			$('#mainNav').addClass('navbar-dark');
 		}
-	});
 
+	});
+	if ($(this).scrollTop() > 5) {
+		$('#mainNav').removeClass('navbar-dark');
+		$('.back-to-top').fadeIn('slow');
+		$('#mainNav').addClass('navbar-light');
+	} else {
+		$('.back-to-top').fadeOut('slow');
+		$('#mainNav').removeClass('navbar-light');
+		$('#mainNav').addClass('navbar-dark');
+	}
 });
 
 function validateEmail(email)
