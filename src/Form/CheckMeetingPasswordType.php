cfdd285d52d1c69519f114dcd7be19c5c6c3761b
ksami <khemsisami@gmail.com>
2020-12-31 15:05:45 +0100

    Implement password meeting check

--- src/Form/CheckMeetingPasswordType.php
+++ src/Form/CheckMeetingPasswordType.php
@@ -4,7 +4,6 @@ namespace App\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
-use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Validator\Constraints\Length;
@@ -15,7 +14,7 @@ class CheckMeetingPasswordType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('password', RepeatedType::class, [
+            ->add('password', PasswordType::class, [
                 'type' => PasswordType::class,
                 'constraints' => [
                     new NotBlank([
