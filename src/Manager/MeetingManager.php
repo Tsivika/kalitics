a37ecec327fcfa7ba3643e122f47b1f405eb997a
ksami <khemsisami@gmail.com>
2021-01-05 13:11:27 +0100

    Update CSS

--- src/Manager/MeetingManager.php
+++ src/Manager/MeetingManager.php
@@ -372,7 +372,7 @@ class MeetingManager extends BaseManager
         $joinMeetingParams->setRedirect(true);
         $joinMeetingParams->setCustomParameter(
             static::CUSTOM_CSS_FIELD_NAME,
-            ':root{--color-primary: #00C9AE;} #message-input:focus{box-shadow: 0 0 0 0;}'
+            ':root{--color-primary: #00C9AE; --btn-primary-bg: #00C9AE;} #message-input:focus{box-shadow: 0 0 0 0;}'
         );
         $joinMeetingParams->setCustomParameter(
             static::CUSTOM_AUTO_SWAP_LAYOUT,
